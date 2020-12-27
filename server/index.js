464a57fa2949f63f145249efdcee50eee790e520
Yunus Emre Gül <yunemregul@gmail.com>
2020-12-27 20:45:33 +0300

    Post request body alma

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 /*
@@ -8,5 +9,10 @@ var app = express();
 app.get("/", function (req, res) {
     res.send("hello world");
 });
+
+app.post("/upload", bodyParser.json(), function (req, res) {
+   
+    console.log(req.body);
+})
 
 app.listen(3000);
